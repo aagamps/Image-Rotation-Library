e27e765577401563e90beac9591edb28f0882409
aagam@kredily.com <Aagam@1395>
2021-05-14 00:35:09 +0530

    Created Image Rotation Library

---
+++ Picture_Rotation_Library/src/main/java/com/android/picture_rotation_library/RotateImage.kt
@@ -0,0 +1,29 @@
+package com.android.picture_rotation_library
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.ExifInterface
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils
+
+class RotateImage {
+
+    internal fun rotateImage(imagePath: String): Bitmap? {
+
+        val options = BitmapFactory.Options()
+        options.inSampleSize = 4
+        val bitmap = BitmapFactory.decodeFile(imagePath, options)
+
+        val orientation: Int = ExifInterface(imagePath).getAttributeInt(
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_UNDEFINED
+        )
+
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> TransformationUtils.rotateImage(bitmap, 90)
+            ExifInterface.ORIENTATION_ROTATE_180 -> TransformationUtils.rotateImage(bitmap, 180)
+            ExifInterface.ORIENTATION_ROTATE_270 -> TransformationUtils.rotateImage(bitmap, 270)
+            ExifInterface.ORIENTATION_NORMAL -> bitmap
+            else -> bitmap
+        }
+    }
+}
